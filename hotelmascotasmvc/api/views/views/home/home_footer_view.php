702e0783114ef378bc2d095b3eaa06ff8be98d6f
RodrigoCanoM <71818543+RodrigoCanoM@users.noreply.github.com>
2021-06-10 12:33:17 +0200

    Delete hotelmascotasmvc/api/views/views directory

--- hotelmascotasmvc/api/views/views/home/home_footer_view.php
+++
@@ -1,6 +0,0 @@
-<footer class="footer-container">
-  <div class="container"> © Copyright <a href="#">www.hotelmascotas.com</a> - Todos los derechos reservados 2021 - <a href="/hotelmascotasmvc/home/contacto">Contacto</a> - <a href="#">Menciones Legales</a> <br>
-  </div>
-</footer>
-</body>
-</html>
